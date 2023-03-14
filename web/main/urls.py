1616448243809a565aabb94e2c8b8cc052fcd9f6
Lachin <l.lachinov@mail.ru>
2023-03-14 22:22:02 +0400

    create front first version

--- web/main/urls.py
+++ web/main/urls.py
@@ -4,5 +4,22 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('about', views.about)
+    path('archive', views.archive, name='archive'),
+    path('auth', views.auth),
+    path('contacts', views.contacts),
+    path('create-news', views.create_news),
+    path('current-news', views.current_news),
+    path('current-manager', views.current_manager),
+    path('editorial-board', views.editorial_board),
+    path('editors', views.editors),
+    path('form-archive', views.form_archive),
+    path('form-contact', views.form_contact),
+    path('form-event', views.form_event),
+    path('form-expert', views.form_expert),
+    path('form-managers', views.form_managers),
+    path('form-news', views.form_news),
+    path('info-for-autors', views.info_for_autors),
+    path('managers', views.managers),
+    path('news', views.news),
+    path('', views.index),
 ]
--- web/main/views.py
+++ web/main/views.py
@@ -6,5 +6,63 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 
+def auth(request):
+    return render(request, 'main/auth.html')
 
+def archive(request):
+    return render(request, 'main/archive.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+def create_news(request):
+    return render(request, 'main/create-news.html')
+
+def current_manager(request):
+    return render(request, 'main/current-manager.html')
+
+def current_news(request):
+    return render(request, 'main/current-news.html')
+
+def editorial_board(request):
+    return render(request, 'main/editorial-board.html')
+
+def editors(request):
+    return render(request, 'main/editors.html')
+
+def footer(request):
+    return render(request, 'main/footer.html')
+
+def form_archive(request):
+    return render(request, 'main/form-archive.html')
+
+def form_contact(request):
+    return render(request, 'main/form-contact.html')
+
+def form_event(request):
+    return render(request, 'main/form-event.html')
+
+def form_expert(request):
+    return render(request, 'main/form-expert.html')
+
+def form_managers(request):
+    return render(request, 'main/form-managers.html')
+
+def form_news(request):
+    return render(request, 'main/form-news.html')
+
+def header(request):
+    return render(request, 'main/header.html')
+
+def info_for_autors(request):
+    return render(request, 'main/info-for-autors.html')
+
+def managers(request):
+    return render(request, 'main/managers.html')
+
+def news(request):
+    return render(request, 'main/news.html')
+
+def pagination(request):
+    return render(request, 'main/pagination.html')
 
